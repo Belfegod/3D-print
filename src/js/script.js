7f75a4fadf45d186f1f7485e03264a977acd23e1
Maksim <blakprinc11@gmail.com>
2022-03-18 17:27:00 +0300

    section 4,5,6,7 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,28 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+$('.btn').on('click', function() {
+  $('.modal').addClass('active');
+});
+$('.overlay').on('click', function() {
+  $('.modal').removeClass('active');
+});
